cfad2154f7a7d09a51985cb8aea99286446fed3a
Reaper <kmr.arnav3484@gmail.com>
2023-12-29 10:36:53 +0530

    Added a grid and implemented basic ant movement

--- main.py
+++ main.py
@@ -1,11 +1,67 @@
 import pygame
 from pygame.locals import *
 from pygame.sprite import Sprite, Group
+import random
+import math
 
-width = 500
-height = 500
+width = 1200
+height = 800
+cell_size = 10
+no_pheremone_color = 'white'
+ant_size = 5
+ant_color = 'red'
+ant_speed = 4
+ant_randomness = 20
 
 screen = pygame.display.set_mode((width, height))
+
+
+class Cell(Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.Surface((cell_size, cell_size))
+        self.image.fill(no_pheremone_color)
+        self.rect = self.image.get_rect(topleft=(x, y))
+
+
+class Ant(Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((ant_size, ant_size))
+        self.image.fill(ant_color)
+        self.rect = self.image.get_rect(center=(500, 500))
+        self.vector = [0, 0]
+
+    def move(self):
+        self.vector = normalize_vector((self.vector[0] + random.randint(-ant_randomness, ant_randomness)/100,
+                                       self.vector[1] + random.randint(-ant_randomness, ant_randomness)/100))
+
+        self.rect.move_ip(self.vector[0] * ant_speed, self.vector[1] * ant_speed)
+
+    def update(self):
+        self.move()
+
+
+def normalize_vector(vector):
+    magnitude = math.sqrt(sum(component ** 2 for component in vector))
+
+    if magnitude != 0:
+        normalized_vector = [component / magnitude for component in vector]
+        return normalized_vector
+    else:
+        return vector
+
+
+ant_group = Group()
+ant = Ant()
+ant_group.add(ant)
+
+# Make grid
+grid_group = Group()
+for i in range(0, width, cell_size):
+    for j in range(0, height, cell_size):
+        cell = Cell(i, j)
+        grid_group.add(cell)
 
 clock = pygame.time.Clock()
 running = True
@@ -16,6 +72,9 @@ while running:
             running = False
 
     screen.fill('pink')
+    grid_group.draw(screen)
+    ant_group.draw(screen)
+    ant_group.update()
 
     clock.tick(60)
     pygame.display.update()
